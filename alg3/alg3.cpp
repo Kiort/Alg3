3ac6a55469aa3dd32ee82047f1b2e81883736d4a
Kiord <slava.vishnj@gmail.com>
2022-03-16 12:58:31 +0300

    Create alg3.cpp

---
+++ alg3/alg3.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <fstream>
+#include <math.h>
+
+using namespace std;
+
+
+int depthGauge() {
+
+    setlocale(LC_ALL, "rus");
+
+    ifstream file;
+    int more = 0;
+    double kord;
+    file.open("C:\\Users\\Asus\\Documents\\Bonch\\sem2\\algoritm\\Alg3\\matrix.txt");
+
+    if (!file)
+    {
+        cout << "Файл не открыт\n\n";
+        return -1;
+    }
+    else
+    {
+        cout << "Все ОК! Файл открыт!\n\n";
+    }
+
+
+    for (file >> kord; !file.eof(); file >> kord) {
+        more++;
+//        cout << kord << "  ";
+    }
+
+
+ //   cout << endl << more<<endl;
+    more = sqrt(more);
+
+    file.close();
+
+  cout <<"В графе "<< more <<" вершин" << endl;
+
+    return more;
+
+}
+
+int main(){
+
+    setlocale(LC_ALL, "rus");
+
+    int more=depthGauge();
+    double** matrix;
+    matrix = new double*[more];
+    for (int i = 0; i < more; i++)
+        matrix[i] = new double[more];
+
+
+    ifstream file;
+    
+    double kord;
+    file.open("C:\\Users\\Asus\\Documents\\Bonch\\sem2\\algoritm\\Alg3\\matrix.txt");
+
+    if (!file)
+    {
+        cout << "Файл не открыт\n\n";
+        return -1;
+    }
+    else
+    {
+        cout << "Все ОК! Файл открыт!\n\n";
+    }
+
+   
+    for (int i = 0; i < more; i++){
+        for (int j = 0; j < more;j++) {
+            file >> matrix[i][j];
+            cout << matrix[i][j]<<"  ";
+        }
+        cout << endl;
+    } 
+
+}
